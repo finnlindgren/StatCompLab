5fdc595a824ff0d39ea08caf840b9f3d6e3c3b14
Finn Lindgren <Finn.Lindgren@gmail.com>
2025-04-09 15:37:20 +0100

    Add missing documentation files

---
+++ R/StatCompLab-package.R
@@ -0,0 +1,16 @@
+#' @keywords internal
+"_PACKAGE"
+
+# Since these packages are Imports, we need to import something from them
+#' @importFrom stats dnorm model.matrix optim qt
+#' @importFrom bench mark
+#' @importFrom gradethis grade_this
+#' @importFrom learnr tutorial_options
+#' @importFrom patchwork plot_layout
+#' @importFrom rmarkdown render
+#' @importFrom spatstat.geom ellipse
+#' @importFrom spatstat.univar ewcdf
+#' @importFrom testthat test_that
+#' @importFrom tidyverse tidyverse_packages
+#' @importFrom knitr opts_chunk
+NULL
